28023edf3986d52e9647e79317df9efe67597ed1
kvchs <15680488752@163.com>
2018-07-01 20:08:32 +0800

    init commit

--- src/main/java/cn/gloryroad/util/KeyBoardUtil.java
+++ src/main/java/cn/gloryroad/util/KeyBoardUtil.java
@@ -1,7 +1,22 @@
 package cn.gloryroad.util;
 
-public class KeyBoardUtil {
-    public static void main(String[] args) {
+import java.awt.*;
+import java.awt.event.KeyEvent;
 
+public class KeyBoardUtil {
+
+    //按Tab键的方法
+    public static void pressTabKey() {
+        Robot robot = null;
+        try {
+            robot = new Robot();
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+        //调用keyPress方法实现按下Tab键
+        assert robot != null;
+        robot.keyPress(KeyEvent.VK_TAB);
+        //调用keyRelease方法实现释放Tab键
+        robot.keyRelease(KeyEvent.VK_TAB);
     }
 }
